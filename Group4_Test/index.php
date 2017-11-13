4b53d3d5b424f253238430a3a273eaaf52d55679
pemmons <pemmons@csumb.edu>
2017-11-13 17:04:45 +0000

    added logout function, register user to index, change button color

--- Group4_Test/index.php
+++ Group4_Test/index.php
@@ -9,6 +9,10 @@
     if(!isset($_SESSION["user"])) {  //Check whether the admin has logged in
         $_SESSION["name"] = "Guest";
         //alert("user is not logged in");
+        
+    }else{
+      
+      
     }
 
     if(isset($_POST['logout'])){
@@ -19,6 +23,11 @@
     }
     
     if(isset($_POST['login'])){
+        echo "goMain <br>";
+        goMain();
+    }
+    
+    if(isset($_POST['register'])){
         echo "goMain <br>";
         goMain();
     }
@@ -36,33 +45,62 @@
   <body>
    
         <header id="title">~ Movie Insight ~
-            <form method ="POST" id="logoutBtn" >
-              <input type="submit" value="Logout" class="btnAD btn" name="logout"/>
-            </form>
+        <?php
+            if(isset($_SESSION['user'])){
+            echo '<form method ="POST" id="logoutBtn" >';
+            echo  '<input type="submit" value="Logout" class="btnAD btn" name="logout"/>';
+            echo '</form>';
+            }
+        ?>
         </header>
         
         <?php
           include 'inc/nav.php';
         ?>
-        <div class= "wrapper">
+        <div class= "wrapper" style="width: 50% !important">
         <h4 id="welcome">Welcome </h4>
         <div id="id02" class="">
+          <div class="containerAD">
+          <table width="100%">
+            <tr>
+                <td>
           <form method="POST" class="" name="loginForm">
     
-            <div class="containerAD">
+            
               <label><b>Username</b></label>
               <input type="text" placeholder="Enter Username" name="username" required id="ittAD">
         
               <label><b>Password</b></label>
               <input type="password" placeholder="Enter Password" name="password" required id="itpAD">
                 
-              <input type="submit" name ="login" value="Login" class="btnAD btn" />
-              <a href='register.php'><div class="btnAD btn">Register</div></a>
+              <input type="submit" name ="login" value="Login" class="btnAD btn sub" />
+            
             </div>
         
           </form>
-          
-        </div>    
+          <td>
+             <form method="POST" class="" name="registerForm">
+               
+              <label><b>Username</b></label>
+              <input type="text" placeholder="Enter Username" name="username" required id="ittAD">
+        
+              <label><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="password1" required id="itpAD">
+              
+              <label><b>Retype Password</b></label>
+              <input type="password" placeholder="Retype Password" name="password2" required id="itpAD">
+                
+              <label><b>Name</b></label>
+              <input type="text" placeholder="Enter Name" name="name" required id="ittAD">
+              
+              <label><b>eMail</b></label>
+              <input type="text" placeholder="Enter eMail" name="email" required id="ittAD">
+                
+              <input type="submit" name ="register" value="Register" class="btnAD btn sub" />
+          </td>
+          </tr>
+        </div> 
+        </table>
         </div>
     <?php
         include 'inc/footer.php';
