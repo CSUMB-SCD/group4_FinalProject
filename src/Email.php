9c4a5a432d9ebdf4477e6761f508544017273f82
pemmons <pemmons@csumb.edu>
2017-12-14 23:39:03 +0000

    test unit test

---
+++ src/Email.php
@@ -0,0 +1,37 @@
+<?php
+
+
+final class Email
+{
+    private $email;
+
+    private function __construct($email)
+    {
+        $this->ensureIsValidEmail($email);
+
+        $this->email = $email;
+    }
+
+    public static function fromString($email)
+    {
+        return new self($email);
+    }
+
+    public function __toString()
+    {
+        return $this->email;
+    }
+
+    private function ensureIsValidEmail($email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '"%s" is not a valid email address',
+                    $email
+                )
+            );
+        }
+    }
+}
+    ?>
