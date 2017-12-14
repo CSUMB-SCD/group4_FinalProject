149c8166d439cdebedfc8471b878e290f5a3c551
Jessie Dowding <jdowding@csumb.edu>
2017-12-14 22:55:13 +0000

    testing test

---
+++ tests/adminReportSourceTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+include 'inc/adminReportSource.php';
+
+function test_numUser()
+{
+    //should return a base number
+    //currently have 3 admin and 2 user
+    $this->assertCount(0, numUser());
+}
+
+?>
