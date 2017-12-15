8434308776477584ef8ff187dc401773fde2bdde
pemmons <pemmons@csumb.edu>
2017-12-15 05:35:49 +0000

    refix the fix

--- tests/checkMessagesTest.php
+++ tests/checkMessagesTest.php
@@ -6,6 +6,8 @@ final class checkMessagesTest extends TestCase
     
     public function CensorshipTestIsNotNull()
     {
+        $mock = $this->getMockBuilder('\checkMessage')
+        ->getMock();
         $this->isNull(
             'myMessage',
             checkMessage::censorship($swearWord)
@@ -14,6 +16,8 @@ final class checkMessagesTest extends TestCase
     
     public function CensorshipTestassertEquals()
     {
+        $mock = $this->getMockBuilder('\checkMessage')
+        ->getMock();
         $this->assertEquals(
             'apple',
             checkMessage::findSwearwords($message)
