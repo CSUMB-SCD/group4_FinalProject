805ea58d61ab4615770588dfec670eceb3d0388d
pemmons <pemmons@csumb.edu>
2017-12-15 05:22:20 +0000

    rename test

--- tests/checkMessagesTest.php
+++ tests/checkMessagesTest.php
@@ -12,7 +12,7 @@ final class adminReportTest extends TestCase
         );
     }
     
-    public function CensorshipTestIsNotNull()
+    public function CensorshipTestassertEquals()
     {
         $this->assertEquals(
             'apple',
